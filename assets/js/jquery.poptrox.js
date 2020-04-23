1074c92a7473d9428e237564d389932194545d74
rlindsberg <roderick@karlemstrand.com>
2020-04-23 21:12:44 +0200

    queue is loaded, move old code here

--- assets/js/jquery.poptrox.js
+++ assets/js/jquery.poptrox.js
@@ -885,6 +885,27 @@
 
 
         });
+
+        //queue is loaded
+        //get url parameter
+        let queryString = window.location.search;
+        let urlParams = new URLSearchParams(queryString);
+        let img_url = urlParams.get('img_url')
+
+        console.log("The image url I read is: " + img_url + "\n");
+        console.log("https://photography.karlemstrand-test.cf" + img_url);
+
+        for (const i in queue) {
+            console.log(i.src);
+        }
+
+        //console.log("the position is " + pos);
+
+
+
+
+
+
         console.log(queue);
         debugger;
 
