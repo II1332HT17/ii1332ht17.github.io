68e791d64d6f7c7cb5656ab21a16d190cb96853e
Bengt Molin <45035172+II1332HT17@users.noreply.github.com>
2020-04-24 16:09:12 +0200

    Pull from origin #c742079157a71d3639dc1f2ad66b095ab354e824 (#4)

--- assets/js/main.js
+++ assets/js/main.js
@@ -253,7 +253,7 @@
                         $image.trigger('click');
                     });
 
-            // EXIF data
+            // EXIF data					
             EXIF.getData($image_img[0], function () {
                 exifDatas[$image_img.data('name')] = getExifDataMarkup(this);
             });
@@ -267,7 +267,7 @@
                 var $image_img = $a.children('img');
                 var data = exifDatas[$image_img.data('name')];
                 if (data === undefined) {
-                    // EXIF data
+                    // EXIF data					
                     EXIF.getData($image_img[0], function () {
                         data = exifDatas[$image_img.data('name')] = getExifDataMarkup(this);
                     });
