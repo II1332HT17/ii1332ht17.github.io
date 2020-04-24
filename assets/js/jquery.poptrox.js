61412b7283f3600468e4c7d02ed70343eafd2230
rlindsberg <roderick@karlemstrand.com>
2020-04-24 15:25:24 +0200

    change http to https

--- assets/js/jquery.poptrox.js
+++ assets/js/jquery.poptrox.js
@@ -892,7 +892,7 @@ function goTo(page, title, url) {
             // Fix src if protocol is 'file'.
             if (window.location.protocol == 'file:'
                 &&	x.src.match(/^\/\//))
-                x.src = 'http:' + x.src;
+                x.src = 'https:' + x.src;
 
             queue.push(x);
 
